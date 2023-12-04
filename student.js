f64bceed34e2dd435d9d70c1552fe70d93a86599
Abdul Wahab Amir <83786802+A-Wahab-Aamir@users.noreply.github.com>
2023-12-04 12:29:47 +0500

    updated json work (filter data)

---
+++ student.js
@@ -0,0 +1,46 @@
+var data=[
+
+{
+    "id":"1",
+    "Student_Name":"Ahmed Ali",
+    "Father_Name":"Yaqoob Ali",
+    "Class":"VB",
+    "Contact":"031192834",
+    "image":"student/s1.jpg"
+},
+{
+    "id":"2",
+    "Student_Name":"Tayab",
+    "Father_Name":"Ansar",
+    "Class":"VB",
+    "Contact":"031192834",
+    "image":"student/s2.jpg"
+},
+{
+    "id":"3",
+    "Student_Name":"Asif",
+    "Father_Name":"Babar Nazeer",
+    "Class":"VB",
+    "Contact":"031192834",
+    "image":"student/s3.jpg"
+},
+{
+    "id":"4",
+    "Student_Name":"Muhammad Arham",
+    "Father_Name":"Muhammad Asif",
+    "Class":"VB",
+    "Contact":"031192834",
+    "image":"student/s4.jpg"
+},
+{
+    "id":"5",
+    "Student_Name":"Naveed",
+    "Father_Name":"Abdul Majeed",
+    "Class":"VB",
+    "Contact":"031192834",
+    "image":"student/s5.jpg"
+},
+
+
+
+]
